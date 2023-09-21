ea37440b5fc2d2b606835a7e589fef7186ca00d6
psuzn <mail@sujanpoudel.me>
2023-09-21 20:37:31 +0545

    CI setup

--- buildSrc/src/main/kotlin/Artifacts.kt
+++ buildSrc/src/main/kotlin/Artifacts.kt
@@ -1,8 +1,8 @@
 object Artifact {
   const val APP_ID = "me.sujanpoudel.playdeals"
   const val APP_NAME = "App Deals"
-  const val VERSION_CODE = 16
-  const val VERSION_NAME = "1.1.5"
+  const val VERSION_CODE = 17
+  const val VERSION_NAME = "1.1.6"
   const val MAJOR_RELEASE = true
 
   const val ANDROID_COMPILE_SDK = 34
