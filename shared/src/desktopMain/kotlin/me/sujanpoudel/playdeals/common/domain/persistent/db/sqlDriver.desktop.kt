9c1836408d024bc7f000e704a9668156aa40aac8
Sujan Poudel <mail@sujanpoudel.me>
2023-10-03 16:44:05 +0545

    fix sqlite migration issue on desktop (#45)

--- shared/src/desktopMain/kotlin/me/sujanpoudel/playdeals/common/domain/persistent/db/sqlDriver.desktop.kt
+++ shared/src/desktopMain/kotlin/me/sujanpoudel/playdeals/common/domain/persistent/db/sqlDriver.desktop.kt
@@ -6,6 +6,8 @@ import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import me.sujanpoudel.playdeals.common.Constants
 import me.sujanpoudel.playdeals.common.SqliteDatabase
 
-actual fun createSqlDriver(): SqlDriver = JdbcSqliteDriver("jdbc:sqlite:${Constants.DATABASE_NAME}").apply {
-  SqliteDatabase.Schema.synchronous().create(this)
-}
+actual fun createSqlDriver(): SqlDriver = JdbcSqliteDriver(
+  url = "jdbc:sqlite:${Constants.DATABASE_NAME}",
+  schema = SqliteDatabase.Schema.synchronous(),
+  migrateEmptySchema = true,
+)
