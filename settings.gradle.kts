f172b5b82284e50813045141cbbd640df3312b19
psuzn <mail@sujanpoudel.me>
2023-12-01 15:52:52 +0545

    remove dev maven repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,6 @@
 pluginManagement {
   repositories {
     gradlePluginPortal()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     google()
     maven("https://plugins.gradle.org/m2/")
   }
