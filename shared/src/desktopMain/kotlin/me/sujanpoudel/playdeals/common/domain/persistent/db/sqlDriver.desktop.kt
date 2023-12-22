4cdc8e2ab2ef223bb310a1967db36fe3ecbed176
psuzn <mail@sujanpoudel.me>
2023-12-22 10:51:04 +0545

    fix foss build

--- shared/src/desktopMain/kotlin/me/sujanpoudel/playdeals/common/domain/persistent/db/sqlDriver.desktop.kt
+++ shared/src/desktopMain/kotlin/me/sujanpoudel/playdeals/common/domain/persistent/db/sqlDriver.desktop.kt
@@ -5,16 +5,15 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import me.sujanpoudel.mputils.paths.appDataDirectory
 import me.sujanpoudel.mputils.paths.utils.div
+import me.sujanpoudel.playdeals.common.BuildKonfig
 import me.sujanpoudel.playdeals.common.Constants
 import me.sujanpoudel.playdeals.common.SqliteDatabase
-import me.sujanpoudel.playdeals.common.BuildKonfig
 
 actual fun createSqlDriver(): SqlDriver {
-
   val path = appDataDirectory(BuildKonfig.PACKAGE_NAME) / Constants.DATABASE_NAME
 
   return JdbcSqliteDriver(
-    url = "jdbc:sqlite:${path}",
+    url = "jdbc:sqlite:$path",
     schema = SqliteDatabase.Schema.synchronous(),
     migrateEmptySchema = true,
   )
